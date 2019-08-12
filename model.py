4f41c5595300512613667d677c4e119194294ee6
vagrant <vagrant@vagrant.vm>
2019-08-12 17:16:20 +0000

    started server

--- model.py
+++ model.py
@@ -19,9 +19,9 @@ class User
     email = db.Column(db.String(100), nullable=False)
     username = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(50), nullable=False)\
-    fav_sport = db.Column(db.String(50), nullable=True)
-
-    photo = db.Column(db.String(100), nullable=True)
+    bio = db.Column(db.String(50), nullable=True)
+   
+    photo_url = db.Column(db.String(100), nullable=True)
 
 
 
@@ -32,11 +32,10 @@ class User
 
 
 
-
 class Event(db.Model):
     """event table"""
 
-    __tablename__ = "events"
+    __tablename__ = "event"
 
     
     event_id = db.Column(db.Integer, primary_key=True,
@@ -51,14 +50,16 @@ class Event(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'),
                         nullable=False)
     sport_id = db.Column(db.Integer, db.ForeignKey('sport.sport_id'),
-                        nullable=False)
+                        nullable=False) #sport is massured in what event they attend to.
 
 
 
     def __repr__(self):
         """ show info aobut human"""
-        return "<event id={} title={} description={} location={}> startData={} endDate={}".format(
-            self.event_id, self.title, self.description, self.location, self.start_date, self.end_date)
+        return ("<event id={} title={} description={}"
+                " location={}> startData={} endDate={}").format(
+            self.event_id, self.title, self.description, 
+            self.location, self.start_date, self.end_date)
 
 
 
@@ -68,10 +69,12 @@ class Sport(db.Model):
 
     __tablename__ = "sport"
 
-    fav_sport = db.Column(db.String(50), nullable=True)
+    sport_id = db.Column(db.Integer, primary_key=True,
+                        autoincrement=True)
 
-    fav_sport = db.Column(db.Integer, db.ForeignKey('even.fav_sport '),
-                        nullable=False)
+    sport_name = db.Column(db.String(50), nullable=True)
+
+    
 
 
 
@@ -83,10 +86,42 @@ class Register(db.Model):
     __tablename__ = "register"
 
 
-    fav_sport = db.Column(db.Integer, db.ForeignKey('even.fav_sport '),
+    
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_idr '),
                         nullable=False)
 
-    fav_sport = db.Column(db.Integer, db.ForeignKey('even.fav_sport '),
+    event_id = db.Column(db.Integer, db.ForeignKey('event.event_id'),
                         nullable=False)
+
+#Helper functions
+
+def init_app():
+    # So that we can use Flask-SQLAlchemy, we'll make a Flask app.
+    from flask import Flask
+    app = Flask(__name__)
+
+    connect_to_db(app)
+    print("Connected to DB.")
+
+
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our database.
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres:///Newbee'
+    app.config['SQLALCHEMY_ECHO'] = False
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+
+    init_app()
+
+
+
 
 
