a52132c726cfc3eb138a89c510e3f3edbce1c102
Hadis Daqiq <hadisdaqiq@Hadiss-MBP.attlocal.net>
2019-09-16 01:57:09 -0700

    .DS_Store banished

---
+++ newbee.sql
@@ -0,0 +1,38 @@
+
+
+INSERT INTO sports (sport_name, img_url) VALUES ('Waterpolo', 
+    'https://total-waterpolo.com/wp-content/uploads/2019/07/AUS-SRB116_optimized-1024x648-960x648.jpg');
+
+INSERT INTO sports (sport_name, img_url) VALUES ('Yoga', 
+    'https://live.staticflickr.com/4782/40015720485_9049da8c55.jpg');
+
+ INSERT INTO sports (sport_name, img_url) VALUES ('Flag Football', 
+    'https://www.flickr.com/photos/mattpendletonphotography/5247809780/in/photolist-8ZJp1Y-8ZJrc3-8ZJzcG-a5N4Fv-7aMJSU-72JfcL-eNhGBj-8ZFmZx-eN6j9D-8ZFq2z-8ZJz33-341yZN-26YRrDD-eNhyuo-eN6idT-8ZJy45-eN6eM2-8ZJzaE-8ZFpSa-eNhwQu-eN6ceX-8ZJKfY-73UxNJ-csXN6b-W1Dx8X-88T54o-oz53Bt-nWQRGm-82NTyr-3LtsGD-csXoFA-8PaTo1-25X4v8H-csXS41-e6oDSj-89z1Qv-253hqc7-csXtWs-csXJDJ-csXn17-csXF6G-csXves-8dK8BE-puBac4-cp4Re9-csXFwq-cPWehL-csXMus-pZLuWu-csXA3S');
+
+
+ INSERT INTO sports (sport_name, img_url) VALUES ('Soccer', 
+        'https://images.unsplash.com/photo-1552667466-07770ae110d0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80');
+
+ INSERT INTO sports (sport_name, img_url) VALUES ('Tennis', 
+        'https://live.staticflickr.com/7111/7566738436_65de220ecb.jpg');
+
+INSERT INTO sports (sport_name, img_url) VALUES ('Hiking', 
+        'https://live.staticflickr.com/3500/3845264221_1435f80f18.jpg');
+
+INSERT INTO sports (sport_name, img_url) VALUES ('Volleyball', 
+    'https://live.staticflickr.com/1105/1417628059_a3161a19ec.jpg');
+
+INSERT INTO sports (sport_name, img_url) VALUES ('Basketball', 
+    'https://live.staticflickr.com/3421/3222974360_3b9a746a54_b.jpg');
+
+INSERT INTO sports (sport_name, img_url) VALUES ('Yoga', 
+    'https://live.staticflickr.com/4782/40015720485_9049da8c55.jpg');
+
+INSERT INTO sports (sport_name, img_url) VALUES ('Swimming', 
+    'https://live.staticflickr.com/7297/10758254444_d624a9c142.jpg');
+
+
+INSERT INTO sports (sport_name, img_url) VALUES ('Lacrosse', 
+    'https://live.staticflickr.com/8793/17626791542_197d62cdf9.jpg');
+
+
